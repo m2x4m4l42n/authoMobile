eecafc64af1624941c0ce583d95e39733a16a7ec
Maximilian von Allwoerden <maximilian.von-allwoerden@stud.uni-due.de>
2019-10-10 15:44:03 +0200

    changed package name

--- app/src/androidTest/java/com/mva/authomobile/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mva/authomobile/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myfirstapp;
+package com.mva.authomobile;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
--- app/src/test/java/com/mva/authomobile/ExampleUnitTest.java
+++ app/src/test/java/com/mva/authomobile/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myfirstapp;
+package com.mva.authomobile;
 
 import org.junit.Test;
 
